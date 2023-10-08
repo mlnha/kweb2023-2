bc32f8d9999c909aa4f36d060221d106d5e480c5
we090@naver.com <we090@naver.com>
2023-10-08 20:58:55 +0900

    exercise2-2

---
+++ chap02/exercise2-2.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const dirList = util.promisify(fs.readdir); //디렉토리 목록 읽어오기
+const fileStat = util.promisify(fs.stat);  //디렉토리 정보 읽어오기
+const PATH = './test';
+
+const directorySearch = async diretory => {
+    const files = await dirList(directory); // 디렉토리 내용 읽고 결과 -> 배열에 저장
+
+    files.forEach(async file => {
+        const filePath = path.join(directory, file); //파일 경로 문자열로 return
+        const stat = await fileStat(filePath);
+
+        if (stat.isDirectory()) await directorySearch(filePath);
+        else if (path.extname(filePath) === '.js') console.log(filePath);
+    });
+};
