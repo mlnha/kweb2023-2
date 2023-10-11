cec5cf2a89b0ba6e6a671b49ebd70c96a32879f3
we090@naver.com <we090@naver.com>
2023-10-11 23:51:43 +0900

    1012

--- chap03/exercise3-2.js
+++ chap03/exercise3-2.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/board/page/:page', (req, res) => {
-    const { page } = req.params;
+    const { page } = req.params; // URL의 가변적인 부분을 변수 params로 저장
     res.send(`This is page #${page}`);
 });
 
--- chap03/exercise3-4.js
+++ chap03/exercise3-4.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set('views', `${__dirname}/views`);
-app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views`); //views 디렉토리를 템플릿 파일을 모아둔 디렉토리로 설정
+app.set('view engine', 'pug'); //view engine으로 pug 사용
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //body 데이터를 받기 위한 미들웨어
 
-app.get('/', (req, res) => res.render('login.pug'));
-app.post('/login', (req, res) => {
-    const { username, password } = req.body; //body에서 데이터 가져옴
+app.get('/', (req, res) => res.render('login.pug')); //사용자에게 login.pug 템플릿 렌더링해서 보여줌
+app.post('/login', (req, res) => { // /login 경로에 대한 POST 요청 (로그인 양식 제출 시 실행)
+    const { username, password } = req.body; //body에서 username, password 값을 가져옴 
     return res.send(`Username: ${username} / Password: ${password}`);
 });
 
